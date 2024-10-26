0a535ac0024d78862b945db01a8fed5e71d88627
Shiwani Kadu <149806001+ShiwaniKadu@users.noreply.github.com>
2024-10-26 22:31:24 +0530

    Update multerConfig.js

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -1,20 +1,18 @@
 const multer = require('multer');
 const path = require('path');
 
-// Configure storage for multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Save files in 'uploads/' directory
+    cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); // Rename file to avoid collisions
+    cb(null, Date.now() + path.extname(file.originalname)); 
   },
 });
 
-// Create the multer instance with file filter and limits
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 5 * 1024 * 1024 }, 
   fileFilter: (req, file, cb) => {
     const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
     if (allowedTypes.includes(file.mimetype)) {
