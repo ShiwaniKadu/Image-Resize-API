3fbce779703a9da817b370a96de671db6760e2b1
Shiwani Kadu <149806001+ShiwaniKadu@users.noreply.github.com>
2024-10-26 22:34:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const path = require('path');
 const imageRoutes = require('./routes/imageRoutes');
@@ -7,13 +6,10 @@ const errorHandler = require('./utils/errorHandler');
 const app = express();
 const PORT = 3000;
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// Routes
 app.use('/api', imageRoutes);
 
-// Global Error Handler
 app.use(errorHandler);
 
 app.listen(PORT, () => {
